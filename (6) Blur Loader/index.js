f1015095d21556811cccf571389b38ed49f04205
CNcoooder <93920553+CNcoooder@users.noreply.github.com>
2022-03-19 23:48:23 +0800

    Add files via upload

---
+++ (6) Blur Loader/index.js
@@ -0,0 +1,17 @@
+const background = document.querySelector('.bg')
+const text = document.querySelector('.text')
+
+function scale(number,inMin,inMax,outMin,outMax){
+    return(number-inMin)*(outMax-outMin)/(inMax-inMin)+outMin
+}
+
+let i = 0
+
+let time = setInterval(() => {
+    i++
+    if(i > 99) clearInterval(time)
+
+    text.innerHTML = `${i}%`
+    text.style.opacity = scale(i,0,100,1,0)
+    background.style.filter = `blur(${scale(i,0,100,30,0)}px)`
+},30)
